eb0bfb1f66ea5f9670d43f061a4ab7064f24106b
thnguyen101 <nguyennt11032004@gmail.com>
2025-01-02 15:50:28 +0700

    add common config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,18 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
+import { ExtraOptions, RouterModule } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
+
+const routeConfig: ExtraOptions = {
+  onSameUrlNavigation: 'reload',
+  scrollPositionRestoration: 'enabled'
+}
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    importProvidersFrom(RouterModule.forRoot(routes, routeConfig)),
+    provideHttpClient()
+  ]
 };
